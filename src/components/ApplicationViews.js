666365471f5c5c336798ecf77cff6c4043ca08aa
Travis Stevenson <thetechtravis@outlook.com>
2021-02-10 15:49:12 -0600

    Ch. 8, complete

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,8 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { GameList } from "./game/GameList.js"
 import { GameProvider } from "./game/GameProvider.js"
+import { EventList } from "./game/EventList.js"
+import { EventProvider } from "./game/EventProvider.js"
 
 export const ApplicationViews = () => {
     return <>
@@ -9,11 +11,19 @@ export const ApplicationViews = () => {
             margin: "5rem 2rem",
             lineHeight: "1.75rem"
         }}>
+            {/* GameList Route */}
             <GameProvider>
                 <Route exact path="/">
                     <GameList />
                 </Route>
             </GameProvider>
+
+            {/* EventList Route */}
+            <EventProvider>
+                <Route exact path="/events">
+                    <EventList />
+                </Route>
+            </EventProvider>
         </main>
     </>
 }
